81a0387be00af1d2b2808fc5c4389edd77b816fd
Willian Ricardo <willianricardoleal@gmail.com>
2023-06-04 22:51:37 -0300

    criando propriedades

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json";
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  question:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.question = quizz_questions.questions
+      this.questionSelected = this.question[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.question.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    console.log(this.answers)
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.answerSelected = this.question[this.questionIndex]
+    }else{
+      this.finished = true
+      //verificar opção ganhadora
+    }
+  }
+
+}
